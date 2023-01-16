437a406ebb072c74664a3b7059f4bca3b95450bb
abdalmajid abdalla <maged.school@hotmail.com>
2023-01-16 11:59:20 +0100

    Adding spring-boot project with web, jpa and lombok\

---
+++ src/main/java/mj249/sd/transportation/sudantransportationcore/SudanTransportationCoreApplication.java
@@ -0,0 +1,13 @@
+package mj249.sd.transportation.sudantransportationcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudanTransportationCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SudanTransportationCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mj249/sd/transportation/sudantransportationcore/SudanTransportationCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package mj249.sd.transportation.sudantransportationcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudanTransportationCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
